af0bf9756243611a12eaa71a267387c5c3cdcc6c
toshichanapp <toshichanapp@gmail.com>
2020-02-11 18:34:12 +0900

    継承の導入

--- movie.rb
+++ movie.rb
@@ -3,29 +3,55 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
+  attr_writer :price
 
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == NEW_RELEAS && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
+  end
+end
+
+module DefaultPrice
+  def frequent_renter_points(_)
+    1
+  end
+end
+
+class RegularPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+
+  def charge(days_rented)
+    days_rented * 3
+  end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
+end
+
+class ChildrenPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
   end
 end
