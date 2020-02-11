f6cd9f3f5b467f1bc9c3225dda6daac7c8a8aa37
toshichanapp <toshichanapp@gmail.com>
2020-02-11 16:12:54 +0900

    金額計算ルーチンの移動

--- customer.rb
+++ customer.rb
@@ -14,8 +14,6 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
-
       # レンタルポイントを加算
       frequent_renter_points += 1
       # 新作2日間レンタルでボーナス点を加算
@@ -23,26 +21,11 @@ class Customer
         frequent_renter_points += 1
       end
       # このレンタルの料金を表示
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount += element.charge
     end
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
-    result
-  end
-
-  def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
     result
   end
 end
