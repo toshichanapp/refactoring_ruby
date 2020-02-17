d39c33a0c6af75a4375eb930b35f5d00ab0033aa
toshichanapp <toshichanapp@gmail.com>
2020-02-17 21:22:15 +0900

    refactor

--- chapter6/substitute_algorithm.rb
+++ chapter6/substitute_algorithm.rb
@@ -1,15 +1,12 @@
 def found_friends(people)
-  friends = []
-  people.each do |person|
-    if(person == 'Don')
-      friends << 'Don'
-    end
-    if(person == 'John')
-      friends << 'John'
-    end
-    if(person == 'Kent')
-      friends << 'Kent'
-    end
+  people.select do |person|
+    %w(Don John Kent).include? person
   end
-  friends
+end
+
+# railsであれば
+def found_friends(people)
+  people.select do |person|
+    person.in?(%w(Don John Kent))
+  end
 end
