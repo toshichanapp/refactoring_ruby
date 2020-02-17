26aa22f137bbcbb3cfea4dd112602c75f877c738
toshichanapp <toshichanapp@gmail.com>
2020-02-17 21:17:31 +0900

    substitute_algrithm

---
+++ chapter6/substitute_algorithm.rb
@@ -0,0 +1,15 @@
+def found_friends(people)
+  friends = []
+  people.each do |person|
+    if(person == 'Don')
+      friends << 'Don'
+    end
+    if(person == 'John')
+      friends << 'John'
+    end
+    if(person == 'Kent')
+      friends << 'Kent'
+    end
+  end
+  friends
+end
