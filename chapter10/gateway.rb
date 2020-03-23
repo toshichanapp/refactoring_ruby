94adff8e7734e61343748e3d9040bf24f1a4995f
toshichanapp <toshiki.fujiyoshi@enechange.co.jp>
2020-03-23 13:41:30 +0900

    GetGateway PostGateway

--- chapter10/gateway.rb
+++ chapter10/gateway.rb
@@ -8,16 +8,34 @@ class Gateway
   end
 
   def execute
+    request = build_request
+    request.basic_auth 'username', 'password' if authenticate
+    Net::HTTP.new(url.host, url.port).start do |http|
+      http.request(build_request)
+    end
+  end
+
+  def url
+    URI.parse(to)
+  end
+end
+
+class PostGateway < Gateway
+  def build_request
     request = Net::HTTP::Post.new(url.path)
     attribute_hash = attributes.inject({}) do |result, attribute|
       result[:attribute.to_s] = subject.send attribute
       result
     end
     request.set_form_data(attribute_hash)
-    Net::HTTP.new(url.host, url.port).start{ |http| http.request(request) }
   end
+end
 
-  def url
-    URI.parse(to)
+class GetGateway < Gateway
+  def build_request
+    parameters = attributes.collect do |attribute|
+      "#{attribute}=#{subject.send(attribute)}"
+    end
+    Net::HTTP::Get.new("#{url.path}?#{parameters.join('&')}")
   end
 end
--- chapter10/introduce_gateway.rb
+++ chapter10/introduce_gateway.rb
@@ -14,9 +14,11 @@ class Company
   attr_accessor :name, :tax_id
 
   def save
-    url = URI.parse('')
-    request  = Net::HTTP::Get.new(url.path, "?name=#{name}&#{tax_id}")
-    Net::HTTP.new(url.host, url.port).start{ |http| http.request(request) }
+    GetGateway.save do |persist|
+      persist.subject = self
+      persist.attributes = [:name, :tax_id]
+      persist.to = ''
+    end
   end
 end
 
@@ -24,13 +26,11 @@ class Laptop
   attr_accessor :assigned_to, :serial_number
 
   def save
-    url = URI・Parse('http://www.example.com/person')
-    request = Net::HTTP::Post.new(url.Path)
-    request.basic_auth('username', 'password')
-    request.set_form_data(
-      'assigned_to' => assigned_to,
-      'serial_number' => serial_number
-    )
-    Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
+    PostGateway.save do |persist|
+      persist.subject = self
+      persist.attributes = [:assigned_to, :serial_number]
+      persist.authenticate = true
+      persist.to = ''
+    end
   end
 end
